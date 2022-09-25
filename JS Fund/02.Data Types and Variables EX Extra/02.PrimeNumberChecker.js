504fb6ac726ad5c98d1f81d0a095cd43ff08322d
CEO OF STOICISM <nikola.lukov.2006@gmail.com>
2022-09-25 20:19:05 +0300

    aded

---
+++ JS Fund/02.Data Types and Variables EX Extra/02.PrimeNumberChecker.js
@@ -0,0 +1,101 @@
+function primeNumber(digit)
+{
+    switch(digit)
+    {
+            case 1:
+            console.log('true');
+            break;
+
+            case 2:
+            console.log('true');
+            break;
+
+            case 3:
+            console.log('true');
+            break;
+
+            case 5:
+            console.log('true');
+            break;
+
+            case 7:
+            console.log('true');
+            break;
+
+            case 11:
+            console.log('true');
+            break;
+
+            case 13:
+            console.log('true');
+            break;
+
+            case 17:
+            console.log('true');
+            break;
+
+            case 19:
+            console.log('true');
+            break;
+
+            case 23:
+            console.log('true');
+            break;
+
+            case 29:
+            console.log('true');
+            break;
+
+            case 31:
+            console.log('true');
+            break;
+
+            case 37:
+            console.log('true');
+            break;
+
+            case 41:
+            console.log('true');
+            break;
+
+            case 43:
+            console.log('true');
+            break;
+
+            case 47:
+            console.log('true');
+            break;
+
+            case 53:
+            console.log('true');
+            break;
+
+            case 59:
+            console.log('true');
+            break;
+
+            case 61:
+            console.log('true');
+            break;
+
+            case 67:
+            console.log('true');
+            break;
+
+            case 71:
+            console.log('true');
+            break;
+
+            case 73:
+            console.log('true');
+            break;
+
+            case 79:
+            console.log('true');
+            break;
+
+            default:
+            console.log('false');
+            break;
+    }
+}
